8a917f5facbba1cc3e17520124019f692f296a55
karamTahhan <kata07001@utb.vaxjo.se>
2024-12-13 08:07:57 +0100

    Add project files.

---
+++ Nivå1 loppar_1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System; 
+
+namespace Loppar_1
+{
+    class program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <=10 ; i++)
+            {
+                Console.WriteLine(i);
+
+            }
+
+
+        }
+
+
+
+    }
+
+
+}
